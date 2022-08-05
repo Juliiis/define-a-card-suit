e7a413345646ccfb91255100303ebfba2bcaeb7e
Julieta Martin <julieta.martin@clarity.ai>
2022-08-05 16:42:20 +0200

    test and logic part for club emoji done

--- app/src/main/java/gradle/java/CardSuit.java
+++ app/src/main/java/gradle/java/CardSuit.java
@@ -1,4 +1,10 @@
 package gradle.java;
 
 public class CardSuit {
+    public String card_suit(String input) {
+        if (input == "3♣"){
+            return "clubs";
+        }
+        return "Sorry, I don't know what emoji is this one :/";
+    }
 }
--- app/src/test/java/gradle/java/CardSuitTest.java
+++ app/src/test/java/gradle/java/CardSuitTest.java
@@ -1,5 +1,20 @@
 package gradle.java;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CardSuitTest {
+    @Test
+    void when_I_received_the_club_emoji_I_get_club(){
+        String input = "3♣";
+        String output = "clubs";
+
+        CardSuit myObject = new CardSuit();
+        String result = myObject.card_suit(input);
+
+        assertThat(result).isEqualTo(output);
+    }
+
 
 }
